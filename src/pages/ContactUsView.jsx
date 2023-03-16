37dac03bff9bff974a90695aa8b17fe3ce9c407e
MartaMunMol <marmunmol@gmail.com>
2023-03-16 10:21:22 +0100

    add working routes for contact, event and profile views

---
+++ src/pages/ContactUsView.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function ContactUsView() {
+
+  return (
+    <h1>CONTACT US PLEASE</h1>
+
+  )
+}
+
+export default ContactUsView
---
+++ src/pages/EventView.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function EventView() {
+
+  return (
+    <h1>THIS IS A EVENT PAGE</h1>
+
+  )
+}
+
+export default EventView
