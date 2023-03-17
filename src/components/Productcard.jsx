3f66ba303723214caf268af651d4e8c6b3877dfd
CeliaGC <you@example.comgarciacastillacelia@gmail.com>
2023-03-17 12:34:58 +0100

    method-getall-works

--- src/components/Productcard.jsx
+++ src/components/Productcard.jsx
@@ -1,27 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import Modalproduct from './Modalproduct';
+import ProductHandler from '../handlers/handlers';
 
 const Productcard = () => {
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+      getData();
+    }, []);
+
+    const getData = async () => {
+      const data = await ProductHandler.loadProducts();
+      setProducts(data);
+    };
+
+    let productlist = products
+
   return (
     <>
         <div className="cold-md-4">
+          
         <Modalproduct show={show} handleClose={handleClose} />
-            <Card style={{ width: '18rem' }}>
-            <Card.Header>Título de poción</Card.Header>
-            <Card.Img variant="top" src="src\assets\images\potiondefault.jpg" />
+        {
+          productlist.map(p =>(
+            <Card style={{ width: '18rem' }} key={p.id}>
+            <Card.Header>{p.productName}</Card.Header>
+            <Card.Img variant="top" src={p.image} />
             <Card.Body>
                 <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
+               {p.description}
                 </Card.Text>
                 <Button variant="secondary" onClick={handleShow}>Más detalles</Button>
             </Card.Body>
             </Card>
+          ))
+          
+        }
+
         </div>
     </>
   );
