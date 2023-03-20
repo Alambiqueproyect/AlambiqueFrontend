f1d1d42d4c0903a27f81cd3d7d18b2d27992bd94
MartaMunMol <marmunmol@gmail.com>
2023-03-20 11:49:24 +0100

    fixed render problem

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { Route } from 'react-router-dom'
 import './index.css'
 import Router from './router/router'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
