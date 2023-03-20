ea0b3e9aa293c01f6870999877d161b7a90633e0
MartaMunMol <marmunmol@gmail.com>
2023-03-20 11:50:59 +0100

    trying to fiex route error

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Route } from 'react-router-dom'
 import './index.css'
 import Router from './router/router'
-import './components/styles/footer.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router/>
