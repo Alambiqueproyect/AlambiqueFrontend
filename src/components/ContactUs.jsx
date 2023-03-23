5673350a009d921c72a018371cbbfc80be853aff
PaolaArcePavez <politaarce@gmail.com>
2023-03-23 10:44:18 +0100

    change of styles component contact

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,49 +1,51 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import './styles/contactUs.css'
+import './styles/reset.css'
 
 function ContactUs() {
 
   return (
+    <div className = "ContactUs">
     <div className="FormContainer">
       <Form>
-        <h3 id="h3">Contact With Us</h3>
+        <h3 id="h3">Contacta con Nosotros</h3>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <div className="Contactcontainer">
-            <Form.Label>Name</Form.Label>
+            <Form.Label>Nombre</Form.Label>
             <Form.Control
               className="FormInputContainer"
               type="text"
-              placeholder="Add Name"
+              placeholder="Introduzca su Nombre"
             />
           </div>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <div className="Contactcontainer">
-            <Form.Label>Email</Form.Label>
+            <Form.Label>Correo Electronico</Form.Label>
             <Form.Control
               className="FormInputContainer"
               type="text"
-              placeholder="Add Email"
+              placeholder="Introduzca su Correo Electronico"
             />
           </div>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <div className="Contactcontainer">
-            <Form.Label>Phone</Form.Label>
+            <Form.Label>Móvil</Form.Label>
             <Form.Control
               className="FormInputContainer"
               type="text"
-              placeholder="Add Phone"
+              placeholder="Introduzca su Móvil"
             />
           </div>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <div className="Contactcontainer">
-            <Form.Label>Message</Form.Label>
+            <Form.Label>Mensaje</Form.Label>
             <Form.Control
               className="FormInputContainer"
               as="textarea"
@@ -53,9 +55,10 @@ function ContactUs() {
         </Form.Group>
 
         <Button variant="primary" type="submit">
-          Submit
+          Enviar
         </Button>
       </Form>
+    </div>
     </div>
   );
 }
