8efd37bfe2e2dec4b5f1a5bd3be7c1f364ba6812
Ana Garrido <anagarridocoder@gmail.com>
2023-03-23 15:20:55 +0100

    four tests passed

---
+++ src/components/NewProductForm.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NewProductForm from './NewProductForm';
+import '@testing-library/jest-dom';
+
+
+
+test('There is a button', () => {
+    vi.mock('react-router-dom',() => ({
+        useLoaderData:vi.NewProductForm()
+    }))
+    render (<NewProductForm show={true} />)
+    screen.debug()
+    const button = screen.getByText(/Crear/i);
+    expect(button).toBeInTheDocument();
+  });
