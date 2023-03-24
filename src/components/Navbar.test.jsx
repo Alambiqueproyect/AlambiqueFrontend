dbacc85829f83f90ceb03c5c48d9a18c45c9e07c
Ana Garrido <anagarridocoder@gmail.com>
2023-03-24 12:45:40 +0100

    five tests passed

--- src/components/Navbar.test.jsx
+++ src/components/Navbar.test.jsx
@@ -5,21 +5,7 @@ import '@testing-library/jest-dom';
 
 test('There is a image in the Navbar', () => {
      const resultScreen = render(<Navbar />);
-     // const image = screen.getByAltText('Alambique');
      const image = resultScreen.container.querySelector('#img-logo');
      console.log(image);
      expect(image).toBeInTheDocument();
    });
-
-
-
-/*import Navbar from "./Navbar"; 
-import { render, screen } from "@testing-library/react"; 
-import '@testing-library/jest-dom'
- 
- 
-test('should contain a png element', () => { 
-     render(<Navbar />); 
-     const PNGElement = screen.get(/icons/i); 
-     expect(PNGElement).toBeInTheDocument(); 
-});*/
--- src/components/Productcard.test.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Productcard from './Productcard';
-import '@testing-library/jest-dom';
