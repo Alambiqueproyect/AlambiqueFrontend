787f3b01879885eb5915edc851c356523007f6c5
Ana Garrido <anagarridocoder@gmail.com>
2023-03-27 11:35:44 +0200

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,12 +18,12 @@ function Navbar() {
         alt="Alambique"
         className="icons" />
       </div>
-      {/* <div className={`nav_items ${isExpanded ? "active" : ""}`}>
+      {<div className={`nav_items ${isExpanded ? "active" : ""}`}>
         <Link to="/">Home</Link>
         <Link to="/events" >Events</Link>
         <Link to="/profile" >Profile</Link>
         <Link to="/contact" >Contact</Link>
-      </div> */}
+      </div>}
       <div className="nav_toggle" onClick={handleToggle}>
         <span></span>
         <span></span>
