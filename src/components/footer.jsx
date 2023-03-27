1fed621d3f8a7c506dda0bacdb5ec2b9f393ab5e
MartaMunMol <marmunmol@gmail.com>
2023-03-27 12:33:57 +0200

    fixed footer color

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,11 +8,11 @@ import {
   MDBRow,
   MDBBtn
 } from 'mdb-react-ui-kit';
-// import './styles/footer.css'
+import './styles/footer.css'
 
 export default function App() {
   return (
-    <MDBFooter className='text-center'  color='white' bgColor='dark'>
+    <MDBFooter className='text-center' id='FooterContainer'  color='white' bgColor='dark'>
       <MDBContainer className='p-4'>
         <section className='mb-4'>
           <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
