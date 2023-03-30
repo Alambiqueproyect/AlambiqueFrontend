653a141cf79569ec39d22fc76b87c233ea32408f
Ana Garrido <anagarridocoder@gmail.com>
2023-03-30 22:05:53 +0200

    fixed register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,25 +1,3 @@
-// import { useState } from "react";
-// import "./styles/Register.css";
-// export const RegisterViewComponent = (props) => {
-//     const [userName, setUserName] = useState("");
-//     const [password, setPassword] = useState("");
-//     const handleSubmit = (event) =>{
-//         event.preventDefault();
-//         console.log(userName);
-//     }
-//   return (
-//     <>
-//     <form onSubmit={handleSubmit}>
-//             <label htmlFor="userName">usuario</label>
-//             <input value={userName} type="text" placeholder="Nombre de usuario" id="userName" name="userName" />
-//             <label htmlFor="password">contraseña</label>
-//             <input value={password} type="password" placeholder="**********" id="password" name="password" />
-//             <button>Register</button>
-//         </form>
-//         <button  onClick={()=> props.onFormSwitch('login')}>¿Ya tienes cuenta? Log in aquí</button>
-//     </>
-//   );
-// }
 import { useState } from "react";
 import "./styles/Register.css";
 export const RegisterViewComponent = (props) => {
