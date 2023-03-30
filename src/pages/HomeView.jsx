82b111bb41a8ce980447abfb19afa59abb083081
Ana Garrido <anagarridocoder@gmail.com>
2023-03-30 13:51:52 +0200

    finished homeview

--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/HomeView.css';
 import ProductList from '../components/ProductList';
 import Navbar from '../components/Navbar';
 import CarouselElement from '../components/CarouselElement';
