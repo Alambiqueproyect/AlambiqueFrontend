2d0b8f10ddd7fd236fd5aa22cd2d836374646895
Ana Garrido <anagarridocoder@gmail.com>
2023-03-30 12:12:17 +0200

    edited app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import './App.css'
-import CarouselElement  from './components/CarouselElement'
-import Productlist from './components/ProductList'
-import Footer from './components/Footer.jsx'
-import Navbar from './components/Navbar'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Login from './components/Login'
-import Register from './components/Register'
+import Register from './components/RegisterViewComponent'
 import { useState } from 'react'
-import ProductList from './components/ProductList'
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
   const toggleForm = (formName) =>{
@@ -17,7 +12,6 @@ function App() {
   }
   return (
     < div>
-      <Productlist/>
       {
         currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
       }
