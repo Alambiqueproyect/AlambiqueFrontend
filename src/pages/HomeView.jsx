3fa630dbf1867580f36eef4253aaed712403babe
MartaMunMol <marmunmol@gmail.com>
2023-03-30 17:06:57 +0200

    saving this commit with stable version of the web

---
+++ src/pages/HomeView.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/HomeView.css';
+import ProductList from '../components/ProductList';
+import Navbar from '../components/Navbar';
+import CarouselElement from '../components/CarouselElement';
+import Footer from '../components/Footer';
+function App() {
+    return (
+        < div>
+            <Navbar />
+            <CarouselElement />
+            <ProductList />
+            <Footer />
+        </div>
+    )
+}
+export default App
