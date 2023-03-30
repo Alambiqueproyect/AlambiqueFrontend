52d6ad1c94afe29153f33583c493063e648a9ff6
Ana Garrido <anagarridocoder@gmail.com>
2023-03-30 21:38:04 +0200

    fixed login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import "../components/styles/Login.css";
+import { Link } from "react-router-dom";
 const Login = (props) => {
 const [userName, setUserName] = useState("");
 const [password, setPassword] = useState("");
@@ -6,17 +8,36 @@ const handleSubmit = (event) =>{
     event.preventDefault();
     console.log(user);
 }
-    return (
-      <>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="userName">usuario</label>
-            <input value={userName} type="text" placeholder="Nombre de usuario" id="userName" name="userName" />
-            <label htmlFor="password">contraseña</label>
-            <input value={password} type="password" placeholder="**********" id="password" name="password" />
-            <button>Log in</button>
-        </form>
-            <button onClick={()=> props.onFormSwitch('register')}>Regístrate aquí si no tienes cuenta</button>
-      </>
-    );
-  }
-  export default Login;
+return (
+  <div className="container">
+    <div>
+      <img src="src\assets\logoNegro.png"
+      alt="Alambique"
+      className="image" />
+    </div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="userName">Usuario</label>
+        <input value={userName}
+        type="text"
+        placeholder="Nombre de usuario"
+        id="userName"
+        name="userName"
+        />
+        <label htmlFor="password">Contraseña</label>
+        <input value={password}
+        type="password"
+        placeholder="**********"
+        id="password"
+        name="password"
+         />
+        <button><Link to="/home">Login</Link></button>
+     </form>
+        <button onClick={()=> props.onFormSwitch('register')}>
+          Regístrate aquí si no tienes cuenta
+        </button>
+      </div>
+  </div>
+);
+}
+export default Login;
